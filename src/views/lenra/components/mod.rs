82660bbf1b745ffe28d2c5d21ba95fead2d4f018
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2023-10-23 10:41:52 +0200

    feat: Carousel (#4)

--- src/views/lenra/components/mod.rs
+++ src/views/lenra/components/mod.rs
@@ -3,15 +3,17 @@ use lenra_app::view::View;
 // mod context;
 mod actionable;
 mod button;
+mod carousel;
 
 pub fn get_views() -> Vec<View> {
     let mut views = vec![];
     views.append(&mut actionable::get_views());
     views.append(&mut button::get_views());
+    views.append(&mut carousel::get_views());
     // views.append(&mut context::get_views());
     views
 }
 
 pub fn get_test_views() -> Vec<String> {
-    vec!["lenra/views/actionable".into(), "lenra/views/button".into()]
+    vec!["lenra/views/actionable".into(), "lenra/views/button".into(), "lenra/views/carousel".into()]
 }
